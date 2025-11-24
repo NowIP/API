d30cfd992ca87dd47521fdabed4a02a4f581068b
Linus Fischer <leicraft@leicraftmc.de>
2025-11-24 18:16:34 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { DNSServer } from "./dns-server";
 import { API } from "./api";
 import { DB } from "./db";
 import { ConfigHandler } from "./utils/config";
@@ -19,6 +20,15 @@ class Main {
             parseInt(config.NOWIP_API_PORT ?? "3003"),
             config.NOWIP_API_HOST ?? "0.0.0.0"
         );
+
+        await DNSServer.init({
+            host: config.NOWIP_DNS_HOST ?? "0.0.0.0",
+            port: parseInt(config.NOWIP_DNS_PORT ?? "53"),
+            rootDomain: config.NOWIP_DNS_ROOT_DOMAIN,
+            publicIPv4: config.NOWIP_DNS_PUBLIC_IPV4,
+            publicIPv6: config.NOWIP_DNS_PUBLIC_IPV6
+        });
+        await DNSServer.start();
 
     }
 
