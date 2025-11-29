1076053ef8eae2dedb48cef9c70331871aed56bc
Linus Fischer <leicraft@leicraftmc.de>
2025-11-29 15:00:33 +0100

    update

--- src/api/routes/account/model.ts
+++ src/api/routes/account/model.ts
@@ -13,7 +13,13 @@ export namespace AccountModel.GetInfo {
 
 export namespace AccountModel.UpdateInfo {
 
-    export const Body = createUpdateSchema(DB.Schema.users).omit({
+    export const Body = createUpdateSchema(DB.Schema.users, {
+        username: z.string()
+            .min(5, 'Must be at least 5 characters')
+            .max(30, 'Must be at most 30 characters')
+            .regex(/^[a-zA-Z0-9_]+$/, 'Only alphanumeric characters and underscores are allowed'),
+        email: z.email('Invalid email'),
+    }).omit({
         id: true,
         password_hash: true
     }).partial();
@@ -26,7 +32,14 @@ export namespace AccountModel.UpdatePassword {
 
     export const Body = z.object({
         current_password: z.string().describe("Current password of the account"),
-        new_password: z.string().min(8).describe("New password for the account (at least 8 characters)")
+        new_password: z.string()
+            .min(8, 'Must be at least 8 characters')
+            .max(50, 'Must be at most 50 characters')
+            .regex(/[A-Z]/, 'Must contain at least one uppercase letter')
+            .regex(/[a-z]/, 'Must contain at least one lowercase letter')
+            .regex(/[0-9]/, 'Must contain at least one number')
+            .regex(/[\W_]/, 'Must contain at least one special character')
+            .describe("New password for the account")
     });
 
     export type Body = z.infer<typeof Body>;
