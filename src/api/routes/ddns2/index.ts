dbd4d9dbe5b5a38b8fb7918bc07b0b653739d08f
Linus Fischer <leicraft@leicraftmc.de>
2026-09-19 10:43:30 +0200

    update

--- src/api/routes/ddns2/index.ts
+++ src/api/routes/ddns2/index.ts
@@ -84,6 +84,7 @@ router.get(
 		const dataWithOneIP = DDNS2Model.Update.QueryWithOneIP.safeParse(body);
 		const dataWithBothIPs = DDNS2Model.Update.QueryWithBothIPs.safeParse(body);
 		const dataWithBothIPsInOneQuery = DDNS2Model.Update.QueryWithBothIPsInOneQuery.safeParse(body);
+		console.log(dataWithBothIPsInOneQuery)
 
 		const hostname: string = body.hostname;
 		let myipv4: string | null = null;
